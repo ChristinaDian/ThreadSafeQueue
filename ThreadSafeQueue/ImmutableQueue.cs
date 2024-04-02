a50ccbbf89e1c08cf6bf6b52e85a3fb7018746c2
ChristinaDian <krisdian2001@gmail.com>
2024-04-02 16:57:36 +0300

    Add project files.

---
+++ ThreadSafeQueue/ImmutableQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSafeQueue
+{
+    class ImmutableQueue<T>
+    {
+        T data;
+        ImmutableQueue<T>? next;
+
+        public ImmutableQueue(T value)
+        {
+            data = value;
+            next = null;
+        }
+        public ImmutableQueue<T> Enqueue(T value)
+        {
+            if (this.next != null)
+            {
+                this.next.Enqueue(value);
+            }
+            else
+            {
+                var newQueue = new ImmutableQueue<T>(value);
+                this.next = newQueue;
+            }
+            return this;
+        }
+        public ImmutableQueue<T>? Dequeue(out T value)
+        {
+            value = data;
+            return next;
+        }
+        public ImmutableQueue<T>? Peek(out T value)
+        {
+            value = data;
+            return this;
+        }
+    }
+}
---
+++ ThreadSafeQueue/Program.cs
@@ -0,0 +1,24 @@
+﻿using ThreadSafeQueue;
+
+namespace BThreadSafeQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImmutableQueue<string> queue = new("abc");
+            queue = queue
+                .Enqueue("cde")
+                .Enqueue("fgh");
+
+            queue = queue
+                .Dequeue(out string val);
+            Console.WriteLine(val);
+            queue = queue
+               .Peek(out val);
+            Console.WriteLine(val);
+
+            Console.ReadLine();
+        }
+    }
+}
